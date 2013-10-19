9be95a09386b3e7f7684452e3d75dbacb8be8582
johnny-h4wk <johnny.h4wk@gmail.com>
2013-10-20 00:35:17 +0200

    first change

--- testgit/src/testgit/testfirst.java
+++ testgit/src/testgit/testfirst.java
@@ -3,8 +3,9 @@ package testgit;
 public class testfirst {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("yoyoyo");
+		System.out.println("yoyoyo2");
+		
 	}
 
 }
